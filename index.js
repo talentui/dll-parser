722e302958fd3a2a8933ae3515afeeeb759fd296
LiGuoming <it1011@qq.com>
2017-10-12 11:09:25 +0800

    更新package name

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
-const namingConvention = require("@beisen/talent-ui-dll-naming-convention");
-const { buildProd, appRoot } = require("./constants.js");
+const namingConvention = require("@talentui/dll-naming");
 
-//解析dll方法
-const parseDll = dllList => {
-  return dllList.map(dll => {
-    if (typeof dll === "object") {
-      return dll;
-    } else if (typeof dll === "string") {
-      let packageName = dll;
-      let version = require(`${dll}/package.json`).version;
-      let { filename, manifest } = namingConvention(
-        packageName,
-        version,
-        buildProd
-      );
-      return {
-        file: `${dll}/dist/${filename}`,
-        manifest: `${dll}/dist/${manifest}`
-      };
-    } else {
-      console.error("dllList 格式不正确");
+module.exports = class DllReference {
+    constructor(dllList, buildProd) {
+        this.rawDllList = dllList;
+        this.buildProd = buildProd;
+        this.dllList = this.parseDll();
     }
-  });
-};
-//生成DllReferencePlugin
-const generateDllReferencePlugins = (dllListAfterParse) => {
-  const DllReferencePlugins = dllListAfterParse.map(dll => {
-    return new (require("webpack")).DllReferencePlugin({
-      manifest: require(dll.manifest),
-      context: appRoot
-    });
-  });
-  return DllReferencePlugins;
-};
 
-module.exports = {
-  parseDll,
-  generateDllReferencePlugins,
-}
+    parseDll() {
+        return this.rawDllList.map(dll => {
+            if (typeof dll === "object") {
+                return dll;
+            } else if (typeof dll === "string") {
+                let packageName = dll;
+                let version = require(`${dll}/package.json`).version;
+                let { filename, manifest } = namingConvention(
+                    packageName,
+                    version,
+                    this.buildProd
+                );
+                return {
+                    file: `${dll}/dist/${filename}`,
+                    manifest: `${dll}/dist/${manifest}`
+                };
+            } else {
+                console.error("dllList 格式不正确");
+            }
+        });
+    }
+
+    getRefPlugin(appRoot) {
+        return this.dllList.map(dll => {
+            return new (require("webpack")).DllReferencePlugin({
+                manifest: require(dll.manifest),
+                context: appRoot
+            });
+        });
+    }
+};
